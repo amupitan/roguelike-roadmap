826c31e4c6f19a0a4b04c9bb106c95cf949a7096
Don-V <donv.amupitan@icloud.com>
2017-01-22 12:01:56 -0600

    Added roads between rooms according to specification

--- game.c
+++ game.c
@@ -4,69 +4,66 @@
 #define nRows 105
 #define nCols 160
 
+typedef struct Point{
+	int x;
+	int y;
+	int immutable;
+}Point;
+
+
 int* create_room(char map[][nCols], int x, int y, int *room_ends);
 int rand_gen(int min, int max);
-int draw_path(char map[][nCols], int x1, int y1, int x2, int y2);
+int connect_cell(char map[][nCols], Point p, Point q);
 
 int main(int argc, char *argv[]){
 	int seed, area = 0, tries = 0, count = 0;
 	char map[nRows][nCols];
-	//static int num_rocks = 0;
 	int i = 0, j = 0;
-//	int[] rooms;
+	Point room_points[35];
 	if (argc > 1)
 		seed = atoi(argv[1]);
 	else seed = (unsigned) time(NULL);
-
 	srand(seed);
+	//initialise all room points to whitespace
 	for (i = 0; i < nRows; i++){
 		for (j =0; j < nCols; j++){
 			map[i][j] = 32;
-			//printf("%c", map[i][j]);
 		}
-//		printf("\n");
 	}
-//	int y = rand()%(nRows-2) + 1;
-//	int x = rand()%(nCols-2) + 1;
-//	int max = nCols-2;
-//	int min = x + 6;
-//	int width = rand_gen(min,max);//(rand() % (max-min+1)) + min;
-//	max = nRows - 2;
-//	min = y + 4;
-//	int height = rand_gen(min,max);//(rand() % (max-min+1)) + min;
-//	for (j = y; j <= height; j++){
-//		for (i = x; i <= width; i++){
-//			map[j][i] = 46;
-//		}
-//	}
-//	int p = create_room(map, 0,0);
-//	int p = rand_gen(3,10);
-	while (area < .2*(nRows*nCols) || tries < 500 || count < 10){
+
+	//add rooms to dungeon
+	while ((area < .2*(nRows*nCols) || tries < 500 || count < 10) && (count < 20)){
 		int y = rand()%(nRows-2) + 1;
 		int x = rand()%(nCols-2) + 1;
 		int room_coords[] = {0,0};
-	       	create_room(map, x, y, room_coords); 
-		area += room_coords[0] * room_coords[1]; 
-		count +=  (area > 0) ? 1 : 0;
+	       	create_room(map, x, y, room_coords);
+		if (room_coords[0] == 0 && room_coords[1] == 0) continue;
+		area += room_coords[0] * room_coords[1];
+
+		//printf("%d < %d\n", x, room_coords[0] + x -1);
+		room_points[count].x = rand_gen(x, room_coords[0] + x -1);
+		room_points[count].y = rand_gen(y, room_coords[1] + y -1);
+		//printf("x %d y %d \n",room_points[count].x, room_points[count].y);
+
+		count +=  (room_coords[0] > 0 && room_coords[1] > 0) ? 1 : 0;//TODO might be able to change this to count++
 		tries++;
 	}
-	draw_path(map, 0, 0, 45,22);
+	//room_points[count].immutable = 1;
+	for (i =0; i < count; i++){
+	//printf("o.x %d o.y %d 1.x %d 1.y %d\n", room_points[0].x, room_points[0].y, room_points[1].x, room_points[1].y);
+		connect_cell(map, room_points[i], room_points[i+1]);
+	}
 	for (i = 0; i < nRows; i++){
 		for (j =0; j < nCols; j++){
 			printf("%c", map[i][j]);
 		}
 		printf("\n");
 	}
-	//printf("x:%d y:%d height:%d width:%d\n", x, y, height, width);
-
-
-
-	return 0;	
+	return 0;
 }
 
 int* create_room(char map[][nCols], int x, int y, int *room_ends){
 	int i = 0, j= 0;
-	//int room_ends[2];
 	//generate random length and width of room by genrating a random co-ord
 	int max = nCols-2;
 	int min = x + 6;
@@ -77,7 +74,7 @@ int* create_room(char map[][nCols], int x, int y, int *room_ends){
 	if (end_x - x > 35 || end_y - y > 25 ) return room_ends;//should be 0
 	if (end_x == -1 || end_y == -1) return room_ends; //should be 0
 
-	//check if this part of dungeon already contains a room	
+	//check if this part of dungeon already contains a room
 	for (i = y-1; i <= end_y + 2; i += end_y - y + 2){ //TODO check logic for this block and the next one
 		for (j = x; j <= end_x; j++){
 			if (map[i][j] == 46) return room_ends;//should be 0
@@ -96,28 +93,28 @@ int* create_room(char map[][nCols], int x, int y, int *room_ends){
 		}
 	}
 
-	//return area of room
+	//return width and length of room respectively
 	room_ends[0] = end_x - x + 1;
        	room_ends[1] = end_y - y + 1;
-	//return (end_x - x + 1) * (end_y - y + 1);
 	return room_ends;
-}	
+}
 
 int rand_gen(int min, int max){
 	return (max >= min) ? (rand() % (max-min+1)) + min : -1;
 }
 
-int draw_path(char map[][nCols], int x1, int y1, int x2, int y2){
-	while (x1 != x2 || y1 != y2){
-		if (map[y1][x1] != 46){
-			map[y1][x1] = '#';
+int connect_cell(char map[][nCols], Point p, Point q){
+	while (p.x != q.x){
+		if (map[p.y][p.x] != 46){
+			map[p.y][p.x] = 35;
 		}
-		if (x1 < x2){
-			x1++;
+		p.x = (p.x > q.x) ? p.x-1 : p.x+1;
+	}
+	while (p.y != q.y){
+		if (map[p.y][p.x] != 46){
+			map[p.y][p.x] = 35;
 		}
-		if (y1 < y2){
-			y1++;
-		}
+		p.y = (p.y > q.y) ? p.y-1 : p.y+1;
 	}
 	return 1;
 }
